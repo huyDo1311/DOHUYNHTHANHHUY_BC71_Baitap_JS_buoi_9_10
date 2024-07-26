62edcad7b64c97c438f93b27a2991c8864fedec6
huyDo1311 <huydo1311.dev@gmail.com>
2024-07-26 23:06:49 +0700

    Resolved merge conflicts main.js

--- js/main.js
+++ js/main.js
@@ -47,100 +47,10 @@ function addStaff() {
 
   if (!isValid) return;
 
-<<<<<<< HEAD
   staffList.addStaffMethod(staff);
   setLocalStorage(staffList.list);
   showStaffList();
   resetForm();
-=======
-  // // Thêm dữ liệu nhân viên mẫu vào danh sách
-  //   const staffData = [
-  //     {
-  //       id: id,
-  //       taikhoan: "0001",
-  //       hoten: "Nguyen Van A",
-  //       email: "nguyenvana@example.com",
-  //       matkhau: "Abc123!",
-  //       ngaylam: "07/09/2024",
-  //       luongcoban: 15000000,
-  //       chucvu: "Nhân viên",
-  //       giolam: 160,
-  //       tongluong: 15000000,
-  //       loainhanvien: "Trung bình",
-  //     },
-  //     {
-  //       id: id,
-  //       taikhoan: "0002",
-  //       hoten: "Nguyen Van B",
-  //       email: "nguyenvanb@example.com",
-  //       matkhau: "Def456!",
-  //       ngaylam: "07/10/2024",
-  //       luongcoban: 17000000,
-  //       chucvu: "Trưởng Phòng",
-  //       giolam: 180,
-  //       tongluong: 34000000,
-  //       loainhanvien: 0,
-  //     },
-  //     {
-  //       id: id,
-  //       taikhoan: "0003",
-  //       hoten: "Nguyen Van C",
-  //       email: "nguyenvanc@example.com",
-  //       matkhau: "Ghi789!",
-  //       ngaylam: "07/11/2024",
-  //       luongcoban: 18000000,
-  //       chucvu: "Sếp",
-  //       giolam: 200,
-  //       tongluong: 54000000,
-  //       loainhanvien: 0,
-  //     },
-  //     {
-  //       id: id,
-  //       taikhoan: "0004",
-  //       hoten: "Nguyen Van D",
-  //       email: "nguyenvand@example.com",
-  //       matkhau: "Jkl012!",
-  //       ngaylam: "07/12/2024",
-  //       luongcoban: 19000000,
-  //       chucvu: "Nhân viên",
-  //       giolam: 150,
-  //       tongluong: 38000000,
-  //       loainhanvien: "Trung bình",
-  //     },
-  //   ];
-
-  // // Thêm dữ liệu nhân viên vào danh sách và lưu trữ vào localStorage
-  //   staffData.forEach((staff) => {
-  //     const newStaff = new Staffs(staff);
-  //     staffList.addStaffMethod(newStaff);
-  //     setLocalStorage(staffList.list);
-  //   });
-
-  var tongLuong = tinhTongLuong(chucvu, luongCB);
-  var xepLoaiNhanVien = xepLoai(chucvu, giolam);
-
-  const staffProps = {
-    id: id,
-    taikhoan: tknv,
-    hoten: name,
-    email: email,
-    matkhau: password,
-    ngaylam: datepicker,
-    luongcoban: luongCB,
-    chucvu: chucvu,
-    giolam: giolam,
-    tongluong: tongLuong,
-    loainhanvien: xepLoaiNhanVien,
-  };
-
-  if (validateStaffProps(staffProps) & validateTaiKhoan(staffProps.taikhoan)) {
-    var staff = new Staffs(staffProps);
-    staffList.addStaffMethod(staff);
-    setLocalStorage(staffList.list);
-    showStaffList();
-    resetForm();
-  }
->>>>>>> ffbdb713107650bea6b56de56a7cd89d12fad04d
 }
 
 function showStaffList() {
